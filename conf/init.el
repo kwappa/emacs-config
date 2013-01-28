968ea6a26aa443a9c03565f45b79132795817185
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-01-29 07:27:41 +0900

    add global behavior settings

--- conf/init.el
+++ conf/init.el
@@ -1,5 +1,39 @@
+;;; behavior settings
+
 ;; load-path
 (add-to-list 'load-path (concat user-emacs-directory "/elisp"))
 
+;; do not create backup file
+(setq auto-save-default nil)            ; # files
+(setq make-backup-files nil)            ; ~ files
+
+;; scroll setting
+(setq scroll-conservatively 35
+      scroll-margin          0
+      scroll-step            1)
+(setq comint-scroll-show-maximum-output t)
+
+;;; display settings
+
+;; show column number
+(column-number-mode t)
+
+;; linum format
+(setq linum-format "%4d ")
+
+;; highlite paren
+(show-paren-mode t)
+
+;;; key settings
+
 ;; C-h : backspace
 (global-set-key "\C-h" 'delete-backward-char)
+
+;; disable commands
+(global-unset-key "\C-xm")              ; compose-mail
+(global-unset-key "\C-z")               ; suspend-frame
+
+;; assign keyboard macro to function keys
+(global-set-key [f3] 'start-kbd-macro)
+(global-set-key [f4] 'end-kbd-macro)
+(global-set-key [f5] 'call-last-kbd-macro)
